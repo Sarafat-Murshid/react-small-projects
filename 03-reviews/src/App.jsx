4850992557605b1e088b1a47b3e6da73b650e289
Sarafat Murshid <sarafatmurshid@gmail.com>
2024-09-24 19:37:34 +0600

    reviews

---
+++ 03-reviews/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import data from "./data";
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from "react-icons/fa";
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = data[index];
+
+  const nextPerson = () => {
+    if (index < data.length - 1) setIndex(index + 1);
+    else setIndex(0);
+  };
+  const prevPerson = () => {
+    if (index > 0) setIndex(index - 1);
+    else setIndex(data.length - 1);
+  };
+
+  return (
+    <>
+      <main>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPerson}>
+              <FaChevronCircleLeft />
+            </button>
+            <button className="next-btn" onClick={nextPerson}>
+              <FaChevronCircleRight />
+            </button>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+};
+export default App;
